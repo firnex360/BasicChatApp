373bb19cd3d5f250e6e757abdc5b6cec9a20c445
mari <mita0001@ce.pucmm.edu.do>
2025-10-03 16:52:59 -0400

    0.1 ADDED +login +manejo de sesiones
    MISSING
    -logout button

--- app/src/main/java/com/example/basicchatapp/MainActivity.kt
+++ app/src/main/java/com/example/basicchatapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.basicchatapp
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,18 +11,24 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.basicchatapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         setSupportActionBar(binding.toolbar)
 
@@ -46,7 +54,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_logout -> {
+                showLogoutDialog()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
@@ -56,4 +67,22 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+
+    private fun showLogoutDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Logout")
+        builder.setMessage("Wanna logout?")
+
+        builder.setPositiveButton("Yes ma'am") { _: DialogInterface, _: Int ->
+            firebaseAuth.signOut()
+            Toast.makeText(this, "damn bro, cya i guess", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+        }
+        builder.setNegativeButton("Nu-uh") { dialog: DialogInterface, _: Int ->
+            dialog.dismiss()
+        }
+        builder.create().show()
+    }
+
 }
